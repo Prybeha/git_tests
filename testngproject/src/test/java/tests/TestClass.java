0450d50f029aa17215c9440db2c81e48ca13149e
Volodymyr Prybeha <vprybeha@dynamo-kiev.local>
2018-11-29 15:03:43 +0200

    good veersion
    good version

--- testngproject/src/test/java/tests/TestClass.java
+++ testngproject/src/test/java/tests/TestClass.java
@@ -2,22 +2,12 @@ package tests;
 
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.*;
-import testlink.api.java.client.TestLinkAPIClient;
-import testlink.api.java.client.TestLinkAPIException;
-import testlink.api.java.client.TestLinkAPIResults;
 import tests.SupportClasses.SetupClass;
+import tests.SupportClasses.TestLinkResults;
 import utils.ExtentsReports.ExtentTestManager;
 
 public class TestClass extends SetupClass {
-    //private TestLinkResults Test = new TestLinkResults(); //object that used for update status in testlink
-    private String TestLink_KEY = "b6b4dca82724b8b027b196c967472932";
-    private String TestLink_URL = "http://localhost/testlink-1.9.18/lib/api/xmlrpc/v1/xmlrpc.php";
-    private String Project_NAME = "FirstProject";
-    private String Plan_NAME = "FirstTestPlan";
-    private String Build_NAME = "FirstBuild";
-
-    private String result = null;
-    private String exception = null;
+    private TestLinkResults Test = new TestLinkResults(); //object that used for update status in testlink
 
     @Test(groups = {"first"})
     public void First() throws Exception{
@@ -26,14 +16,11 @@ public class TestClass extends SetupClass {
         String Expectedtitle = "Google123";
         String Actualtitle = driver.getTitle();
         try {
-            assert Actualtitle.equals(Expectedtitle);
-            result = TestLinkAPIResults.TEST_PASSED;
-            updateTestLinkResult("FirstTestCase", exception, result);
+            //assert Actualtitle.equals(Expectedtitle);
+            Test.TestLinkPass("FirstTestCase");
         }
-        catch(Exception ex){
-            result = TestLinkAPIResults.TEST_FAILED;
-            exception = ex.getMessage();
-            updateTestLinkResult("FirstTestCase", exception, result);
+        catch(AssertionError ex){
+            Test.TestLinkFail("FirstTestCase", ex.getMessage());//Send error massage
         }
     }
 
@@ -42,10 +29,10 @@ public class TestClass extends SetupClass {
         ExtentTestManager.getTest().setDescription("Second test");
         try {
             System.out.println(driver.getCurrentUrl());
-            //Test.TestLinkPass("SecondTestCase");
+            Test.TestLinkPass("SecondTestCase");
         }
         catch(Exception ex){
-            //Test.TestLinkFail("SecondTestCase", ex.getMessage());//Send error massage
+            Test.TestLinkFail("SecondTestCase", ex.getMessage());//Send error massage
         }
     }
 
@@ -59,9 +46,5 @@ public class TestClass extends SetupClass {
         catch(Exception ex){
             //Test.TestLinkFail("ThirdTestCase", ex.getMessage());//Send error massage
         }
-    }
-    private void updateTestLinkResult(String testCase, String exception, String result) throws TestLinkAPIException {
-        TestLinkAPIClient testlinkAPIClient = new TestLinkAPIClient(TestLink_KEY, TestLink_URL);
-        testlinkAPIClient.reportTestCaseResult(Project_NAME, Plan_NAME, testCase, Build_NAME, exception, result);
     }
 }
