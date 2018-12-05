a5a267fcb8c95ab3ae015fdf9dbfa5393e69ad80
Volodymyr Prybeha <vprybeha@dynamo-kiev.local>
2018-12-05 17:58:59 +0200

    update
    update

--- testngproject/src/test/java/tests/SupportClasses/SetupClass.java
+++ testngproject/src/test/java/tests/SupportClasses/SetupClass.java
@@ -2,9 +2,13 @@ package tests.SupportClasses;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxDriverLogLevel;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 public class SetupClass {
     public WebDriver driver;
@@ -14,10 +18,19 @@ public class SetupClass {
         return driver;
     }
 
+    @Parameters("browser")
     @BeforeMethod
-    public void setup () {
-        System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDrivers\\chromedriver.exe");
-        driver = new ChromeDriver();
+    public void setup (String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDrivers\\chromedriver.exe");
+            driver = new ChromeDriver();
+        }
+        else if (browser.equalsIgnoreCase("firefox")){
+            System.setProperty("webdriver.gecko.marionette", "C:\\SeleniumDrivers\\geckodriver.exe");
+            FirefoxOptions options = new FirefoxOptions();
+            options.setLogLevel(FirefoxDriverLogLevel.INFO);
+            driver = new FirefoxDriver(options);
+        }
         wait = new WebDriverWait(driver,5);
         driver.manage().window().maximize();
         driver.get("http://google.in");
