b27f44bd1b14db843bff01494f34aa499f2b83c1
Volodymyr Prybeha <vprybeha@dynamo-kiev.local>
2018-12-06 16:34:42 +0200

    allure
    allure

--- MavenTestProject/src/test/java/TestClass.java
+++ MavenTestProject/src/test/java/TestClass.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -29,6 +30,18 @@ public class TestClass {
         assertEquals(title,"Google123");
         LogUtil.log("Test Passed. Expected: Google | Actual: "+title);
         System.out.println("Page Loaded");
+    }
+
+    @Title("Second test")
+    @Description("Find some thing in google")
+    @Test
+    public void searchSomething(){
+        wait.until(ExpectedConditions.titleIs("Google"));
+        driver.findElement(By.name("q")).sendKeys("Maven");
+        driver.findElement(By.name("btnK")).click();
+        LogUtil.log("Maven page request");
+        assertEquals(driver.getCurrentUrl(),"https://www.google.co.in/search?source=hp&ei=YDIJXLbCIIiTsAG165eQBw&q=Maven&btnK=%D0%9F%D0%BE%D1%88%D1%83%D0%BA+Google&oq=Maven&gs_l=psy-ab.3..0l10.111011.116882..117455...6.0..0.73.790.12......0....1..gws-wiz.....0..0i131j0i10.OIF_djpsDJE");
+        LogUtil.log("URLs are equals.");
     }
 
     @AfterMethod
